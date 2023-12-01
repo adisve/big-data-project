b7a615285b65c70be39c3099f870a10f9c1905e2
PsychicPlatypus <dzenismadzovic@gmail.com>
2023-12-01 21:00:14 +0100

    Add describe to dataframs, markdown for introduction

--- jupyter-notebook/spark/dataset.py
+++ jupyter-notebook/spark/dataset.py
@@ -7,19 +7,25 @@ class Dataset(object):
     Class to contain the dataset and perform processing on it, as well
     as provide access to the dataframes attributes.
     """
+
     def __init__(self, hdfs_path, spark_master):
         self._init_session(spark_master)
         self.hdfs_path = hdfs_path
         self._df = None
 
     def _init_session(self, spark_master):
-        self.spark = SparkSession.builder \
-            .master(spark_master) \
-            .appName("Pre-processing Reddit Comments") \
+        self.spark = (
+            SparkSession.builder.master(spark_master)
+            .appName("Pre-processing Reddit Comments")
             .getOrCreate()
+        )
 
     def pre_process(self):
-        df = self.spark.read.format("parquet").option("header", "true").load(self.hdfs_path)
+        df = (
+            self.spark.read.format("parquet")
+            .option("header", "true")
+            .load(self.hdfs_path)
+        )
         df = df.drop("downs", "ups", "subreddit")
         df = df.filter(df.body != "[deleted]")
         df = df.filter(df.body != "[removed]")
@@ -33,7 +39,7 @@ class Dataset(object):
     @available
     def schema(self):
         return self._df.schema
-    
+
     @property
     @available
     def dataframe(self):
